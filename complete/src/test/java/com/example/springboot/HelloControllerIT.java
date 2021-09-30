c20ed6e0a55ea14f4e6c4e3c43bb4cce0b77db95
Apurva Kansara <apurvakansara5@gmail.com>
2021-09-30 22:14:44 +0530

    full code changed

--- complete/src/test/java/com/example/springboot/HelloControllerIT.java
+++ complete/src/test/java/com/example/springboot/HelloControllerIT.java
@@ -1,14 +1,10 @@
 package com.example.springboot;
-
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
-
 import static org.assertj.core.api.Assertions.assertThat;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HelloControllerIT {
 
