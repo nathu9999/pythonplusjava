1ef889682701088294ae3d0f538d4f8f6a8d6357
DarekKasata <72450944+DarekKasata@users.noreply.github.com>
2021-09-30 21:53:02 +0530

    Scripting tag added

--- test/run.sh
+++ test/run.sh
@@ -6,7 +6,28 @@ cd ../complete
 ./mvnw clean package
 ret=$?
 if [ $ret -ne 0 ]; then
+exit $
+if [ $ret -ne 0 ]; then
 exit $ret
+fi
+rm -rf target
+
+./gradlew build
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf buildif [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf target
+
+./gradlew build
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf buildret
 fi
 rm -rf target
 
@@ -18,6 +39,19 @@ fi
 rm -rf build
 
 cd ../initial
+
+
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf target
+
+./gradlew build
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf build
 
 ./mvnw clean compile
 ret=$?
