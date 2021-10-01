ed7f3ed5d4a51d6f7aabf09a36d3484307193216
DarekKasata <72450944+DarekKasata@users.noreply.github.com>
2021-10-01 09:15:48 +0530

    Post mapping of controller inserted

--- initial/src/main/java/com/example/springboot/HelloController.java
+++ initial/src/main/java/com/example/springboot/HelloController.java
@@ -10,5 +10,9 @@ public class HelloController {
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
+	@PostMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
 
 }
