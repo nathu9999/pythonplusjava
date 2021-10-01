e5be0c7b14a3a4a170e3285db4ef3a4db9807b54
Arnab2010 <72144787+Arnab2010@users.noreply.github.com>
2021-10-01 08:54:46 +0530

    scripts for else and echos added

--- test/run.sh
+++ test/run.sh
@@ -40,31 +40,31 @@ rm -rf build
 
 cd ../initial
 
-
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf targetret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf targetret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf targetret=$?
 if [ $ret -ne 0 ]; then
 exit $ret
 fi
 rm -rf target
-
-./gradlew build
-ret=$?
 if [ $ret -ne 0 ]; then
 exit $ret
 fi
 rm -rf build
-
-./mvnw clean compile
 ret=$?
 if [ $ret -ne 0 ]; then
 exit $ret
 fi
 rm -rf target
-
-./gradlew compileJava
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf build
 
 exit
