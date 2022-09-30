db011a1fc4387c71163c3a7808e081a588f0c768
bosenetaji9999 <91649398+bosenetaji9999@users.noreply.github.com>
2022-10-01 01:46:55 +0530

    java commit for aws

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -5,6 +5,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	
+	int num = 29;
+        boolean flag = false;
+        for(int i = 2; i <= num/2; ++i)
+        {
+            if(num % i == 0)
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
 
 	@GetMapping("/")
 	public String index() {
